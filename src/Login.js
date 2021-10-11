5a33635295fbdb69878aebbb147a521657a67409
Nick Kruize <nick.kruize@student.fontys.nl>
2021-10-11 14:58:51 +0200

    Added session class

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Row, Form, Col, Container, Button } from 'reactstrap';
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.css';
+import {Session} from './Session';
 
 
 export class Login extends Component {
@@ -152,18 +153,18 @@ export class Login extends Component {
     }
 }
 
-class Session extends Map {
-    set(id, value) {
-        if (typeof value === 'object') value = JSON.stringify(value);
-        sessionStorage.setItem(id, value);
-    }
+// class Session extends Map {
+//     set(id, value) {
+//         if (typeof value === 'object') value = JSON.stringify(value);
+//         sessionStorage.setItem(id, value);
+//     }
 
-    get(id) {
-        const value = sessionStorage.getItem(id);
-        try {
-            return JSON.parse(value);
-        } catch (e) {
-            return value;
-        }
-    }
-}
+//     get(id) {
+//         const value = sessionStorage.getItem(id);
+//         try {
+//             return JSON.parse(value);
+//         } catch (e) {
+//             return value;
+//         }
+//     }
+// }
