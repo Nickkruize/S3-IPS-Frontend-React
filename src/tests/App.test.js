369a76748ba3cc68c3d395d4791517d334d03eec
Nick Kruize <n.kruize@gmail.com>
2021-10-11 03:25:47 +0200

    First tests

---
+++ src/tests/App.test.js
@@ -0,0 +1,22 @@
+import {Layout} from '../Layout';
+import {NavMenu} from '../NavMenu';
+import {shallow} from "enzyme";
+import App from '../App';
+
+
+it("renders without crashing", () =>{
+    const wrapper = shallow(<App />);
+    expect(wrapper).toBeTruthy()
+})
+
+it("calls layout at least once", () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find(Layout).length).toEqual(1);
+  });
+
+it("calls Navmenu at least once", () => {
+    const wrapper = shallow(<Layout/>);
+    expect(wrapper.find(NavMenu).length).toEqual(1);
+  });
+
+
